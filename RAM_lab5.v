2df7b72eec31f68e09652ab82ece28e4a09a560f
KhanhEK <107037350+KhanhEK2846@users.noreply.github.com>
2022-11-29 08:26:47 +0700

    Add files via upload

---
+++ RAM_lab5.v
@@ -0,0 +1,23 @@
+module RAM_lab5(clock, cs, wr_e, oe, addr, data);
+input clock, cs, wr_e, oe;
+input [6:0]addr;
+inout reg [7:0]data;
+reg [7:0]mem[6:0];
+
+always@(posedge clock)
+begin
+	if(cs && !oe && wr_e)
+		mem[addr] <= data;
+end
+
+
+always@(*)
+begin
+	if(cs && oe && !wr_e)
+		data <= mem[addr];
+	else
+		data <= 8'bz;	
+		
+end
+
+endmodule
